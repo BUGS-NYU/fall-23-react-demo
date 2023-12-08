4be23e4530874d54373b35c446b8bcf370da4bdb
abigailzhou03 <alz4471@nyu.edu>
2023-12-08 17:05:02 -0500

    session 1

---
+++ demo/src/components/Greet.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+// function Greet() {
+//     return <h1>Hello Abigail</h1>
+// }
+
+export const Greet = () => <h1>Hello Abigail</h1> // named export
+
+//export default Greet
